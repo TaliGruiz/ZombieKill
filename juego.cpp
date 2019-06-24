f1b815163fd476f82a8c8e81decae3d45bf273c9
LucasAbbiatici <49872088+LucasAbbiatici@users.noreply.github.com>
2019-06-24 01:51:18 -0300

    vector balas intento 40

--- juego.cpp
+++ juego.cpp
@@ -34,7 +34,8 @@ survivor pj({ 0,0 }, 1, 1), & ref = pj;
 zombie zombie1({ 200, 300 }, 1, 20, 100);
 Vector2f pjCenter, mousePosWindow, aimDir, aimDirNorm;
 bullet b1;
-
+int contBalas = 0;
+bullet* vecBalas = new bullet[30];
 
 
 juego::juego(Vector2f resolucion, String titulo)
@@ -42,12 +43,9 @@ juego::juego(Vector2f resolucion, String titulo)
 	bool flag = false;
 	ventana1 = new RenderWindow(VideoMode(resolucion.x, resolucion.y), titulo);
 	ventana1->setFramerateLimit(60);
-	
-
 
 	game_over = false;
 	fps = 1 / 60.f;
-
 
 	eventos = new Event;
 	reloj1 = new Clock();
@@ -102,7 +100,14 @@ void juego::gameloop(Vector2f resolucion)
 			///ventana1->draw(spr_intro1);
 
 			ventana1->draw(spr_fondo);
-			ventana1->draw(b1.spr_bala);
+			//ventana1->draw(b1.spr_bala);
+
+			if (contBalas == 30 || Keyboard::isKeyPressed(Keyboard::R)) {
+				delete(vecBalas);
+				contBalas = 0;
+				vecBalas = new bullet[30];
+			}
+
 			///dibujo pj y mira al mouse
 			ventana1->draw(pj.get_spr_survivordisparo());
 			ventana1->draw(pj.get_spr_survivor());
@@ -125,9 +130,9 @@ void juego::gameloop(Vector2f resolucion)
 			///procesar colision pj-ventana
 			pj.colisionVentana(resolucion);
 
-			if (Collision::CircleTest(b1.get_spr_bala(), zombie1.get_spr_zombie())) {
+			/*if (Collision::CircleTest(b1.spr_bala, zombie1.get_spr_zombie())) {
 				cout << "Colision pa" << endl << endl;
-			};
+			};*/
 			
 			///MOVIMIENTO SURVIVOR CON TECLADO
 			pj.movimiento_teclado();
@@ -137,7 +142,8 @@ void juego::gameloop(Vector2f resolucion)
 			zombie1.mover(Vector2f(zombie1.get_velocidad().x, zombie1.get_velocidad().y));
 			
 			///MOVIMIENTO BALA
-			b1.mover(Vector2f(b1.get_velocidad().x, b1.get_velocidad().y));
+
+			//b1.mover(Vector2f(b1.get_velocidad().x, b1.get_velocidad().y));
 			
 			
 			/////////======================////////////////////
@@ -253,9 +259,14 @@ void juego::procesar_eventos()
 						sonidoDisparo.setPitch(7);
 						sonidoDisparo.play();
 						sonidoDisparo.setVolume(50);
-						b1.spr_bala.setColor(Color(255, 255, 255, 255));
+						ventana1->draw(vecBalas[contBalas].spr_bala);
+						vecBalas[contBalas].spr_bala.setColor(Color(255, 255, 255, 255));
+						vecBalas[contBalas].update(pj.get_posicion(), Mouse::getPosition(*ventana1));
+						vecBalas[contBalas].mover(Vector2f(vecBalas[contBalas].get_velocidad().x, vecBalas[contBalas].get_velocidad().y));
+						contBalas++;
+						//b1.spr_bala.setColor(Color(255, 255, 255, 255));
 						//b1.spr_bala.setFillColor(Color(0, 0, 0, 255));
-						b1.update(pj.get_posicion(),Mouse::getPosition(*ventana1));
+						//b1.update(pj.get_posicion(),Mouse::getPosition(*ventana1));
 					}
 				break;
 			}
